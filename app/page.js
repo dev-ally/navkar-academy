3eeeb06e88a755dff614ccbb306ab1fd74957ec0
Aryan shinde <shindearyan179@gmail.com>
2024-08-10 23:28:07 +0530

    Added Header & Footer Component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Navkar Academy</h1>
+      <Footer />
     </div>
   );
 }
