248d6f368856aee611483645da47fa00bf7ccf48
Aryan shinde <shindearyan179@gmail.com>
2024-08-11 00:07:00 +0530

    Completed Header Skeleton

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -1,14 +1,24 @@
 import Image from "next/image";
 import React from "react";
 import Container from "./Container";
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
     <Container>
       <div className="pt-6">
-        <div className="flex flex-col justify-between items-center gap-6 md:gap-16 md:flex-row px-6">
+        <div className="flex justify-between items-center gap-6 md:gap-16 px-6">
+          {/* Logo */}
           <div>
-            {/* Logo */}
             <Image
               src="/demo-logo.svg"
               alt="Demo Logo"
@@ -17,9 +27,21 @@ const Header = () => {
               className="w-full h-[40px]"
             />
           </div>
-          <div className="md:hidden">{/* Mobile Nav */}</div>
+          {/* Mobile Nav */}
+          <div className="md:hidden">
+            <Sheet>
+              <SheetTrigger>
+                <Menu />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Are you absolutely sure?</SheetTitle>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+          {/* CTA Information */}
           <div className="hidden md:items-center md:flex md:gap-10">
-            {/* CTA Information */}
             <div>
               <h3>Call</h3>
               <p>123-456-7890</p>
@@ -31,7 +53,32 @@ const Header = () => {
           </div>
         </div>
         <div className="w-full h-[2px] bg-black/10 rounded-full my-4" />
-        <div className="hidden md:block">{/* Page Links */}</div>
+        <div className="hidden md:block">
+          {/* Page Links */}
+          <div className="flex justify-between items-center px-6">
+            <div className="flex justify-between items-center gap-10 w-[40%]">
+              <div className="">
+                <Link href="/">Home</Link>
+              </div>
+              <div className="">
+                <Link href="/toppers">Toppers</Link>
+              </div>
+              <div className="">
+                <Link href="/events">Events</Link>
+              </div>
+              <div className="">
+                <a href="https://store.navkaracademy.in" target="_blank">
+                  Store
+                </a>
+              </div>
+            </div>
+            <div className="">
+              <div>
+                <Button>Contact Us</Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Container>
   );
