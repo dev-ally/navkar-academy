b1d4dd40a9633907986420b670a592f28f13b853
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2024-08-12 00:21:42 +0530

    changes in events pagee

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Card = ({ eventdate, eventimg }) => {
   return (
-    <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+    <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border  border-2  rounded-lg w-full sm:w-80  lg:w-96 hover:scale-95 transition-transform duration-300 ease-in-out cursor-pointer mx-auto">
       <div>
         <img
           src={eventimg}
           alt="Event Image"
-          className="w-full h-48 object-cover rounded-t-xl"
+          className="w-full h-48 object-cover rounded-t-lg"
         />
       </div>
-      <div class="p-6 pt-3 border-t border-gray-200 ">
-        <p className="text-gray-700 text-center ">{eventdate}</p>
+      <div className="p-6 pt-3 border-t border-gray-200 ">
+        <p className="text-gray-700 text-center font-medium">{eventdate} </p>
       </div>
     </div>
   );
