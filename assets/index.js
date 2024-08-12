dd917b4d4d6911b75d0b0326db0df3c3369b1c81
Aryan shinde <shindearyan179@gmail.com>
2024-08-12 22:18:32 +0530

    Completed About Us

--- assets/index.js
+++ assets/index.js
@@ -6,4 +6,20 @@ import x from "@/assets/social/x.svg";
 import banner from "@/assets/header/banner.png";
 import elements from "@/assets/header/elements.png";
 
-export { facebook, instagram, linkedin, x, banner, elements };
+import education from "@/assets/about/education.png";
+import quality from "@/assets/about/quality.png";
+import results from "@/assets/about/results.png";
+import teacher from "@/assets/about/teacher.png";
+
+export {
+  facebook,
+  instagram,
+  linkedin,
+  x,
+  banner,
+  elements,
+  education,
+  quality,
+  results,
+  teacher,
+};
--- data/aboutCardsData.js
+++ data/aboutCardsData.js
@@ -1,23 +1,23 @@
-import { ThumbsUp } from "lucide-react";
+import { Book, Gem, ShieldQuestion, ThumbsUp } from "lucide-react";
 
 export const aboutCardsData = [
   {
     id: 1,
     title: "Best Education",
-    icon: <ThumbsUp />,
+    icon: <Book />,
     description:
       "We offer exceptional education with a focus on comprehensive learning. Our curriculum is designed to foster your child's intellectual growth and academic excellence.",
   },
   {
     id: 2,
-    title: "Experienced Teachers",
-    icon: <ThumbsUp />,
+    title: "Weekly Doubt Sessions",
+    icon: <ShieldQuestion />,
     description:
-      "Our team of educators is highly skilled and experienced. They bring a wealth of knowledge and dedication to ensure your child receives personalized and effective guidance.",
+      "We conduct weekly doubt sessions to address any questions your child may have. Our educators provide personalized support to ensure thorough understanding and academic progress.",
   },
   {
     id: 3,
-    title: "Best Results",
+    title: "Better Results",
     icon: <ThumbsUp />,
     description:
       "Our commitment to high-quality education and expert teaching has consistently led to outstanding results. We pride ourselves on the academic achievements of our students.",
@@ -25,7 +25,7 @@ export const aboutCardsData = [
   {
     id: 4,
     title: "Quality Classes",
-    icon: <ThumbsUp />,
+    icon: <Gem />,
     description:
       "We deliver top-notch classes that meet high educational standards. Our focus on quality ensures an enriching learning experience for every student.",
   },
