2b90fa99e2de0e8b652861df19acfbbbecb92a1a
Aryan shinde <shindearyan179@gmail.com>
2024-08-14 19:58:52 +0530

    Teachers Section Completed

--- components/private/home/Teachers.jsx
+++ components/private/home/Teachers.jsx
@@ -23,22 +23,26 @@ const Teachers = () => {
             are dedicated to help students learn and grow.
           </p>
         </div>
-        <div className="grid grid-cols-4 gap-10">
-          <div>
-            <Image
-              // src="https://avatar.iran.liara.run/public/boy?username=Ash"
-              src={teacherprof}
-              alt="Teacher Image"
-              width={1000}
-              height={1000}
-              className="w-full h-full"
-            />
-            <h2>Teacher Name</h2>
-            <h4>Subject</h4>
-          </div>
-          <div>Teacher #2</div>
-          <div>Teacher #3</div>
-          <div>Teacher #4</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-14">
+          {[...Array(4)].map((_, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <div className="rounded-t-full border-4 hover:border-accent transition-all duration-300">
+                <div className="p-4 overflow-hidden flex justify-center items-center">
+                  <Image
+                    src={teacherprof}
+                    alt="Teacher Profile"
+                    width={1000}
+                    height={1000}
+                    className="w-[90%] md:w-full"
+                  />
+                </div>
+                <div className="w-full flex justify-center items-center flex-col p-3 pt-0">
+                  <h3 className="text-xl font-semibold">Teacher Name</h3>
+                  <p className="text-base text-center">Subject</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </Container>
